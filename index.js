9468afcbb9a5b6a2959469b464f695f881edc238
Mika Salazar <m.razalas@gmail.com>
2023-06-17 13:47:19 -0400

    added removeTodo function

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ function App() {
     setValue('')
   }
 
+  const removeTodo = (e) => {
+    const index = Number(e.target.id)
+    let temp = [...todos]
+    temp.splice(index, 1)
+    setTodos(temp)
+  }
+
   return (
     <>
       {todos.map((todo, i) => (
-        <div className="todo" key={i}>
+        <div className="todo" key={i} id={i} onClick={removeTodo}>
           {todo.text}
         </div>
       ))}
